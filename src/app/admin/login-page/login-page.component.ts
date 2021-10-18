89fcb16ca60e6c9219f101696042711ac8686476
yuliya.kurchankova <iulia.kurchenkova@yandex.by>
2021-10-18 17:41:07 +0300

    add page

---
+++ src/app/admin/login-page/login-page.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
+})
+export class LoginPageComponent implements OnInit {
+
+   form: FormGroup;
+   submitted: boolean = false;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6)
+        ])
+    })
+  }
+
+  submit() {
+    if(this.form.invalid) {
+      return
+    }
+    this.submitted = true;
+  }
+
+}
